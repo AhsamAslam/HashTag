aa36999739e195ed000487256932761ac9255b33
shahzaib-hub <shahzaib494a@gmail.com>
2020-01-21 21:15:47 +0500

    shahzaib

---
+++ RanKingApp/FFmpegLiabrary/CallFfmpeg.cs
@@ -0,0 +1,24 @@
+﻿using EmergenceGuardian.FFmpeg;
+using System;
+
+namespace FFmpegLiabrary
+{
+    public class CallFfmpeg
+    {
+        public static string CallProcess(string config, string query)
+        {
+            FFmpegConfig.FFmpegPath = config;
+
+            FFmpegProcess ffmpeg = new FFmpegProcess(null);
+            CompletionStatus status = ffmpeg.RunFFmpeg(query);
+            if (status.ToString() == "Success")
+            {
+                return status.ToString();
+            }
+            else
+            {
+                return ffmpeg.Output;
+            }
+        }
+    }
+}
